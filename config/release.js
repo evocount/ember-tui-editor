d478ab3518886913d80d1ecf16c0692c5135f452
Till Theato <theato@ttill.de>
2018-09-08 00:20:46 +0200

    Add ember-cli-release.

---
+++ config/release.js
@@ -0,0 +1,32 @@
+/* jshint node:true */
+// var RSVP = require('rsvp');
+
+// For details on each option run `ember help release`
+module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  publish: true,
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  // timezone: 'America/Los_Angeles',
+  //
+  // beforeCommit: function(project, versions) {
+  //   return new RSVP.Promise(function(resolve, reject) {
+  //     // Do custom things here...
+  //   });
+  // }
+  afterPublish: function(project, versions) {
+    runCommand('ember github-pages:commit --message "Released ' + versions.next + '"');
+    runCommand('git push origin gh-pages:gh-pages');
+  },
+};
+
+
+function runCommand(command) {
+  console.log('Running: ' + command);
+  var output = execSync(command, { encoding: 'utf8' });
+  console.log(output);
+}
