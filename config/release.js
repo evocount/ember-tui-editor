2a84863e8ea11263206ab7e9c408a6be3240dca2
Till Theato <till.theato@evocount.de>
2020-05-06 22:18:47 +0200

    Replace outdated ember-cli-release.

--- config/release.js
+++ config/release.js
@@ -1,37 +1,5 @@
-/* jshint node:true */
-// var RSVP = require('rsvp');
+'use strict';
 
-var execSync = require('child_process').execSync;
-
-// For details on each option run `ember help release`
 module.exports = {
-  // local: true,
-  // remote: 'some_remote',
-  // annotation: "Release %@",
-  // message: "Bumped version to %@",
-  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-
-  // does not work with npm 2fa
-  //publish: true,
-
-  // strategy: 'date',
-  // format: 'YYYY-MM-DD',
-  // timezone: 'America/Los_Angeles',
-  //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
-  afterPublish: function(project, versions) {
-    runCommand('ember github-pages:commit --message "Released ' + versions.next + '"');
-    runCommand('git push origin gh-pages:gh-pages');
-  },
+  strategy: 'semver' // options: semver, date
 };
-
-
-function runCommand(command) {
-  console.log('Running: ' + command);
-  var output = execSync(command, { encoding: 'utf8' });
-  console.log(output);
-}
